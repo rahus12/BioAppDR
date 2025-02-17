41e16ea3d413d579a7295a9bd6635d0fcfb8485e
rahus12 <rahulsamp4@gmail.com>
2025-02-16 20:05:05 -0800

    added another indexcard, made minor changes

--- lib/components/indexcard.dart
+++ lib/components/indexcard.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class IndexCard extends StatelessWidget {
-  const IndexCard({
-    super.key,
-  });
+
+  String? title = "";
+  String? questions;
+  String? progress;
+
+  IndexCard({this.title, this.questions, this.progress});
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +14,13 @@ class IndexCard extends StatelessWidget {
       color: Color(0xFFF5F5F5),
       child: Column(
         children: [
-          Text("Home Page", style: TextStyle(fontSize: 30) ),
           // black box container
+          SizedBox(height: 30,),
           Center(
             child: Container(
-              padding: EdgeInsets.fromLTRB(20 ,10, 20, 5),
+              padding: EdgeInsets.fromLTRB(20 ,10, 20, 10),
               width: 337,
-              height: 126,
+              height: 116,
               decoration: BoxDecoration(
                 color: Colors.black, // Background color
                 borderRadius: BorderRadius.circular(8), // Rounded corners
@@ -26,7 +29,7 @@ class IndexCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Heart Quiz",
+                    "$title",
                     style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.w500,
@@ -34,19 +37,19 @@ class IndexCard extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    "13 Questions",
+                    "$questions Questions",
                     style: TextStyle(
                         fontSize: 14,
                         fontWeight: FontWeight.w500,
                         color: Color(0xFF7C7C7C)
                     ),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(height: 5,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        "Progress 17%",
+                        "Progress $progress%",
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w500,
@@ -57,7 +60,7 @@ class IndexCard extends StatelessWidget {
                           style: ElevatedButton.styleFrom(
                               backgroundColor: Color(0xFFFFD166),
                               foregroundColor: Colors.black,
-                              fixedSize: Size(55, 30),
+                              fixedSize: Size(55, 20),
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(6), // Set the radius here
                               ),
