67ebf1969f18d6dd8c662b7f201b5dc171349966
rahus12 <rahulsamp4@gmail.com>
2025-02-17 00:55:10 -0800

    almost done with the homepage look

--- lib/components/indexcard.dart
+++ lib/components/indexcard.dart
@@ -10,77 +10,73 @@ class IndexCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFFF5F5F5),
-      child: Column(
-        children: [
-          // black box container
-          SizedBox(height: 30,),
-          Center(
-            child: Container(
-              padding: EdgeInsets.fromLTRB(20 ,10, 20, 10),
-              width: 337,
-              height: 116,
-              decoration: BoxDecoration(
-                color: Colors.black, // Background color
-                borderRadius: BorderRadius.circular(8), // Rounded corners
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // black box container
+        SizedBox(height: 10,),
+        Container(
+          padding: EdgeInsets.fromLTRB(20 ,10, 20, 10),
+          width: double.infinity,
+          height: 116,
+          decoration: BoxDecoration(
+            color: Colors.black, // Background color
+            borderRadius: BorderRadius.circular(8), // Rounded corners
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "$title",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Text(
+                "$questions Questions",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF7C7C7C)
+                ),
+              ),
+              SizedBox(height: 5,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "$title",
+                    "Progress $progress%",
                     style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.w500,
-                        color: Colors.white
+                        color: Color(0xFFFFD166)
                     ),
                   ),
-                  Text(
-                    "$questions Questions",
-                    style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF7C7C7C)
-                    ),
-                  ),
-                  SizedBox(height: 5,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Progress $progress%",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFFFFD166)
-                        ),
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFFFFD166),
-                              foregroundColor: Colors.black,
-                              fixedSize: Size(55, 20),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(6), // Set the radius here
-                              ),
-                              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5)
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFFFD166),
+                          foregroundColor: Colors.black,
+                          fixedSize: Size(55, 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6), // Set the radius here
                           ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/question');
-                          },
-                          child: Text("Start", style: TextStyle(
-                            fontSize: 16,
-                          ),)),
-                    ],
-                  ),
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5)
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/question');
+                      },
+                      child: Text("Start", style: TextStyle(
+                        fontSize: 16,
+                      ),)),
                 ],
               ),
-            ),
+            ],
           ),
+        ),
 
-        ],
-      ),
+      ],
     );
   }
 }
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -19,24 +19,94 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("BioApp", style: TextStyle(fontWeight: FontWeight.w500),),
-        backgroundColor: Colors.purple[400],
-        foregroundColor: Colors.white,
+        // backgroundColor: Colors.purple[400],
+        // foregroundColor: Colors.white,
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+              begin: Alignment.topCenter, // Start direction
+              end: Alignment.bottomCenter, // End direction
+              colors: [
+              Color(0xFFE6E1F5), // Start Color
+              Color(0xFFF5F5F5),// End Color
+              ], // Customize your colors here
+              ),
+            ),
+        ),
       ),
-      body: Column(
-        children: [
-          Text("Home Page", style: TextStyle(fontSize: 30) ),
-          IndexCard(
-            title: "Heart Quiz",
-            questions: "13",
-            progress: "17",
-          ),
-          IndexCard(
-            title: "Lorem Ipsum",
-            questions: "23",
-            progress: "0",
-          ),
-        ],
+      body: Container(
+        color: Color(0xFFF5F5F5),
+        padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+        // avatar and title
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hi, Jane",
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 36 * 0.01,
+                      ),
+                    ),
+                    Text("Learn. Play. Grow!", style: TextStyle(
+                      fontSize: 13,
+                      color: Color(0xFF7C7C7C),
+
+                    ),)
+                  ],
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/chunli.jpg'),
+                  radius: 40,
+                )
+              ],
+            ),
+            SizedBox(height: 40,),
+            Text("Explore", style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500
+              )),
+            //The Quiz index cards
+            IndexCard(
+              title: "Heart Quiz",
+              questions: "13",
+              progress: "17",
+            ),
+            IndexCard(
+              title: "Lorem Ipsum",
+              questions: "23",
+              progress: "0",
+            ),
+
+          ],
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+      backgroundColor: Colors.black, // Background color
+      selectedItemColor: Colors.white, // Active item color
+      unselectedItemColor: Colors.white, // Inactive item color
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    ),
     );
   }
 }
